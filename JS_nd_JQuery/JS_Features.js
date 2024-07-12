deafe18e78bc2d6d429390d6e39e2e252221e339
Sourav Saha8 <sourav.saha8@ibm.com>
2024-07-12 15:12:33 +0530

    Adv feature js added

---
+++ JS_nd_JQuery/JS_Features.js
@@ -0,0 +1,13 @@
+//Template Literal
+let str=`This is a template literal`
+console.log(str);
+
+//String Interpolation
+const obj={
+    name:"Pen",
+    Price:10
+}
+let print=`Price of ${obj.name} is ${obj.Price} rupees.`
+console.log(print);
+
+// Array Methods
